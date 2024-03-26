714b5483226c63f629c75659af250a0da404fb06
shubham4556tf <150424128+shubham4556tf@users.noreply.github.com>
2024-03-26 17:02:32 +0530

    Initial commit

---
+++ blog_page.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('.loco'),
+        smooth: true,
+       
+        lerp:0.1
+    });
+
+
+
+})
