f00faa20b337fb0ef5620539caaa50c8779d3707
shubham4556tf <150424128+shubham4556tf@users.noreply.github.com>
2024-03-26 23:57:48 +0530

    change i

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const scroll = new LocomotiveScroll({
-  el: document.querySelector(".data-scroll-container"),
+  el: document.querySelector(".data-scroll-container "),
   smooth: true,
   smartphone: { smooth: true },
   tablet: { smooth: true },
   lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
   multiplier: 1.2, // Effect Multiplier
   });
+  const scroll2 = new LocomotiveScroll({
+    el: document.querySelector(".data-scroll-container_mobile "),
+    smooth: false,
+    tablet: { smooth: true },
+    lerp: 1, // Linear Interpolation, 0 > 1 // Try 0.01
+    // Effect Multiplier
+    });
 
       
     
 setTimeout(() => {  
-    locoScroll.destroy();
+  data-scroll-container.destroy();
 }, 0);
 setTimeout(() => {  
-    locoScroll.init();
+  data-scroll-container.init();
 }, 50);
 setTimeout(() => {  
-    locoScroll.update();
+  data-scroll-container.update();
 }, 1000);
